089cfdcde0a1a0b84cf02d096969fdc191026735
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-27 11:09:27 +0200

    feat: integrate Josefin Sans Google font into RootLayout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,11 @@ import Navigation from '@/app/_components/Navigation';
 
 import '@/app/_styles/globals.css';
 
+import { Josefin_Sans } from 'next/font/google';
+const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
+
+console.log(josefin);
+
 export const metadata = {
   // title: 'The Wild Oasis',
   title: {
@@ -17,8 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className="
-     bg-primary-950 text-primary-100 min-h-screen"
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
       >
         <header>
           <Logo />
