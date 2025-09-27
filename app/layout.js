b417e9c256f5a1012cf4539736a4490cd78baf61
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-27 11:18:47 +0200

    feat: add Header component and update layout structure
    - Move Logo and Navigation into new Header component
    - Update RootLayout to use Header and flex layout for main content
    - Add spacing and max-width constraints for main area
    - Enhance Navigation with hover and transition styles

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
 
 console.log(josefin);
@@ -22,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>&copy; Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
