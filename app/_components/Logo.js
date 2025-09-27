503b61efea3c0c1e5527142f361590ca5338f2cc
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-27 11:42:34 +0200

    feat: refine layout and homepage design
    - Rename logo import for clarity in Logo component
    - Update RootLayout body to include relative positioning
    - Implement homepage hero section with background Image, heading, and call-to-action link

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,15 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import logo from '@/public/logo.png';
+import logoImage from '@/public/logo.png';
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
-      {/* <Image src={logo} alt="The Wild Oasis logo" /> */}
       <Image
-        src={logo}
+        src={logoImage}
         quality={100}
         height="60"
         width="60"
--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,6 @@ const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
 console.log(josefin);
 
 export const metadata = {
-  // title: 'The Wild Oasis',
   title: {
     template: '%s / The Wild Oasis',
     default: 'Welcome / The Wild Oasis',
@@ -23,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
