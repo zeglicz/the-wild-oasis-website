365e7ae5baec3596c68a33f37ccac7f2a2217510
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-27 12:29:22 +0200

    feat: add account sub-layout and side navigation
    - Add account layout with grid for side navigation
    - Implement SideNavigation component with links to Home, Reservations, and Guest profile
    - Add SignInButton and SignOutButton components
    - Update profile and reservations pages metadata
    - Adjust RootLayout main wrapper to full width

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,8 @@
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
+import { Josefin_Sans } from 'next/font/google';
+
+import Header from './_components/Header';
 
 import '@/app/_styles/globals.css';
-
-import { Josefin_Sans } from 'next/font/google';
-import Header from './_components/Header';
 const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
 
 console.log(josefin);
@@ -25,8 +23,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
