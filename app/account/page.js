83066676c3dc6d7d8c67ce1cdfabaa35dd33b612
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-27 12:11:58 +0200

    feat: build account area skeleton
    - Add account page greeting
    - Add profile page with form and country selector
    - Add reservations page with ReservationCard list
    - Add ReservationCard and DeleteReservation components
    - Install date-fns

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,11 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Account Page</h1>;
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Przemek
+      </h2>
+    </div>
+  );
 }
