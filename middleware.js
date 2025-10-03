e0ecfa558071a1620e4f2d599e12eb5117a462d1
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-10-03 10:56:43 +0200

    feat: protect /account route with NextAuth middleware
    - Added authorized callback in auth.js to allow only authenticated users
    - Introduced middleware.js using auth() to secure /account route

---
+++ middleware.js
@@ -0,0 +1,15 @@
+// middleware by default run for every single route
+/*
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  console.log(request);
+
+  return NextResponse.redirect(new URL('/about', request.url));
+}
+*/
+
+import { auth } from '@/app/_lib/auth';
+export const middleware = auth;
+
+export const config = { matcher: ['/account'] };
