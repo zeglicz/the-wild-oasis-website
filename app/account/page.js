c1fdabb766cfe86ad4b76d7749f20fd3ca90fc93
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-10-03 11:14:58 +0200

    feat: custom login flow with Google sign-in
    - Added signInAction server action to handle Google login and redirect to /account
    - Updated SignInButton to use form submission with signInAction
    - Configured NextAuth to use custom /login page for signIn
    - Implemented /login page with SignInButton
    - Updated account page to greet user by first name from session

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,17 @@
+import { auth } from '@/app/_lib/auth';
+
 export const metadata = {
   title: 'Guest area',
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(' ').at(0);
+
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Przemek
+        Welcome, {firstName}
       </h2>
     </div>
   );
